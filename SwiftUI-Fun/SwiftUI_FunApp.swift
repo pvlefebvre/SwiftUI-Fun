63db96f07ee640fc7665dbfec7ab3c2e563cef1a
pvlefebvre <paul.v.lefebvre@gmail.com>
2023-02-08 20:41:57 -0500

    Initial Commit

---
+++ SwiftUI-Fun/SwiftUI_FunApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FunApp.swift
+//  SwiftUI-Fun
+//
+//  Created by Paul Lefebvre on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
